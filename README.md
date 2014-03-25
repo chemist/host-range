eb7aaffb0ad5b31d9fd14f82c64fa94450501393
Alexey Smirnov <chemistmail@gmail.com>
2014-03-25 14:23:00 +0400

    add network server

--- README.md
+++ README.md
@@ -6,8 +6,29 @@ Simple util for generate host range.
 cabal build
 cabal install
 
-Usage:
+Usage as util:
 echo "[aaa-000 aaa-010, bb00 bb05]" | ./host-range
+
+Usage as network server:
+./host-range -d
+open 4000 port
+work with memcached protocol
+
+session example:
+ ~ -$ telnet localhost 4000                                                                                                                                  limbo-air@chemist :(
+Trying ::1...
+telnet: connect to address ::1: Connection refused
+Trying 127.0.0.1...
+Connected to localhost.
+Escape character is '^]'.
+GET [ab-001 ab-010, host01.example.com host05.example.com]
+VALUE [ab-001 ab-010, host01.example.com host05.example.com] 0 166
+[ab-001,ab-002,ab-003,ab-004,ab-005,ab-006,ab-007,ab-008,ab-009,ab-010,host01.example.com,host02.example.com,host03.example.com,host04.example.com,host05.example.com]
+END
+Connection closed by foreign host.
+
+
+
 
 Output: ["aaa-000","aaa-001","aaa-002","aaa-003","aaa-004","aaa-005","aaa-006","aaa-007","aaa-008","aaa-009","aaa-010","bb00","bb01","bb02","bb03","bb04","bb05"]
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Control.Applicative ((<$>), (<*>), (*>), (<*), many, pure, (<|>))
 import Data.Attoparsec.Text (Parser, digit, takeWhile, space, char, many1, inClass, many', sepBy', parseOnly)
-import Data.Text (Text, justifyRight, pack)
+import Data.Text (Text, justifyRight, pack, take, drop, init, length)
 import Data.Monoid ((<>))
 import Data.Char (digitToInt)
 import Data.Text.IO (getLine)
-import Prelude hiding (getLine, takeWhile)
+import Data.Conduit (await, yield, (=$), ($$), ($=), Conduit)
+import Data.Conduit.Text (decodeUtf8, encodeUtf8, linesBounded)
+import Data.Conduit.Network (runTCPServer, appSource, appSink, Application, HostPreference( HostAny ), serverSettings)
+import System.Environment (getArgs)
+import System.Exit (exitSuccess, exitFailure)
+import qualified Prelude as P
+import Prelude hiding (getLine, takeWhile, take, drop, init, length)
+
+main :: IO ()
+main = doWork =<< getArgs 
+    where
+      doWork [] = do
+          s <- getLine
+          case work s of
+               Right x -> (print x) >> exitSuccess
+               Left a -> (print $ "bad hosts range " <> a) >> exitFailure
+      doWork x = case head x of
+                     "-s" -> server
+                     _ -> (print "bad usage") >> exitFailure
 
 data Host = Host Text Int Int Text 
           | Range Host Host
@@ -20,19 +39,18 @@ eval (Enum x) = concatMap eval x
 tT :: Int -> Text
 tT = pack . show
 
-
 host :: Parser Host
 host = do
     p <- preds
     d <- many digit
     e <- ends
-    return $ Host p (length d) (digitsToInt d) e
+    return $ Host p (P.length d) (digitsToInt d) e
     where
       digitsToInt i = sum . map (uncurry (*)) $ zip razr (rv i)
       rv = reverse . map digitToInt
       razr = [1,10,100,1000,10000,100000,1000000,10000000]
       preds = takeWhile (inClass $ ['A' .. 'Z'] ++ ['a' .. 'z'] ++ "-")
-      ends = takeWhile (inClass $ ['A' .. 'Z'] ++ ['a' .. 'z'] ++ "-.")
+      ends = takeWhile (inClass $ ['A' .. 'Z'] ++ ['a' .. 'z'] ++ ".-")
 
 range :: Parser Host
 range = Range <$> (host <* many1 space ) <*> host 
@@ -44,12 +62,35 @@ enum :: Parser Host
 enum = Enum <$> (range <|> host ) `sepBy'` separator
 
 hosts :: Parser Host
-hosts = char '[' *> enum <* char ']'
+hosts = many' space *> char '[' *> many' space *> enum <* many' space <* char ']' <* many' space
 
-main :: IO ()
-main = do
-    s <- getLine
-    case eval <$> parseOnly hosts s of
-         Right x -> print x
-         Left _ -> print "bad hosts range"
-    
+work :: Text -> Either String [Text]
+work s = eval <$> parseOnly hosts s
+
+server :: IO ()
+server = runTCPServer (serverSettings 4000 HostAny) hostRangeApp
+
+hostRangeApp :: Application IO 
+hostRangeApp appData = do
+    let source = appSource appData
+        sink = appSink appData
+    source $= decodeUtf8 $= linesBounded 67 $$ conduit =$ encodeUtf8 =$ sink
+
+conduit :: Conduit Text IO Text
+conduit = do
+    l <- await
+    case take 4 <$> l of
+         Just "get " -> runHost (init . drop 4 <$> l)
+         Just "GET " -> runHost (init . drop 4 <$> l)
+         _ -> yield "CLIENT_ERROR only get request\r\n"
+    where
+      lTt x = "[" <> foldl1 (\x y -> x <> "," <> y) x <> "]"
+      sizeD = pack . show . length . lTt
+      runHost Nothing = yield "CLIENT_ERROR \r\n"
+      runHost (Just str) = case work str of
+                         Left _ -> do 
+                             yield $ "CLIENT_ERROR unused format\r\n"
+                         Right x -> do
+                             yield $ "VALUE " <> str <> " 0 " <> sizeD x <> " \r\n"
+                             yield $ lTt x <> "\r\n"
+                             yield "END\r\n"
