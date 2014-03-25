1f36e13e443d684fc592143fcf68af040313fb95
Alexey Smirnov <chemistmail@gmail.com>
2014-03-25 12:05:00 +0400

    fix point and remove unused types

--- src/Main.hs
+++ src/Main.hs
@@ -7,18 +7,12 @@ import Data.Char (digitToInt)
 import Data.Text.IO (getLine)
 import Prelude hiding (getLine, takeWhile)
 
-type Predicate = Text
-type End = Text
-type Len = Int
-
-data Host = Host Predicate Len Int End 
+data Host = Host Text Int Int Text 
           | Range Host Host
           | Enum [Host] 
           deriving (Show, Eq)
 
-type SHost = Text
-
-eval :: Host -> [SHost]
+eval :: Host -> [Text]
 eval (Host p l i e) = [p <> justifyRight l '0' (tT i) <> e]
 eval (Range (Host p l i e) (Host _ _ i1 _)) = concatMap (\x -> eval (Host p l x e)) [i .. i1]
 eval (Enum x) = concatMap eval x
@@ -31,14 +25,14 @@ host :: Parser Host
 host = do
     p <- preds
     d <- many digit
-    e <- preds
+    e <- ends
     return $ Host p (length d) (digitsToInt d) e
     where
-      digitsToInt :: String -> Int
       digitsToInt i = sum . map (uncurry (*)) $ zip razr (rv i)
       rv = reverse . map digitToInt
       razr = [1,10,100,1000,10000,100000,1000000,10000000]
       preds = takeWhile (inClass $ ['A' .. 'Z'] ++ ['a' .. 'z'] ++ "-")
+      ends = takeWhile (inClass $ ['A' .. 'Z'] ++ ['a' .. 'z'] ++ "-.")
 
 range :: Parser Host
 range = Range <$> (host <* many1 space ) <*> host 
